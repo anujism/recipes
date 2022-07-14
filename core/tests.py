0ac2bd4e6476d367422c581f461d77cebd2548c5
Anuj Agarwal <anuj.ism@gmail.com>
2022-07-14 15:44:25 +0530

    change test

--- core/tests.py
+++ core/tests.py
@@ -10,10 +10,10 @@ class TestIngredient:
     @pytest.mark.django_db
     def test_ingredient_properties(self):
         # 1 kg of ingredient1 cost 100 euros
-        ingredient_1 = IngredientFactory(cost=100, unit="kg", quantity=1)
+        ingredient_1 = IngredientFactory(cost=1000, unit="kg", quantity=1)
         # 1 litre of ingredient2 cost 10 euros
         ingredient_2 = IngredientFactory(cost=10, unit="l", quantity=1)
-        assert ingredient_1.quantity_in_si_unit == Decimal(1000)
+        assert ingredient_1.quantity_in_si_unit == Decimal(10000)
         assert ingredient_2.quantity_in_si_unit == Decimal(1)
         assert ingredient_1.cost_per_si_unit == Decimal('0.1')
         assert ingredient_2.cost_per_si_unit == Decimal(10)
