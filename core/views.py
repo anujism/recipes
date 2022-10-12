0cbfd005bfc049ee4e1df787dee1b565d461606c
Anuj Agarwal <anuj.ism@gmail.com>
2022-10-12 11:27:06 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -4,13 +4,6 @@ from django.http import JsonResponse
 from core.models import Ingredient, get_unit_choices
 
 
-def some_other_method_is_required(request, ingredient_id=None):
-    ingredient = Ingredient.objects.get(id=ingredient_id)
-    return JsonResponse({
-        "ingredient": {"id": ingredient.id},
-    })
-
-
 @staff_member_required
 def units_by_ingredient(request, ingredient_id=None):
     # TODO: handle wrong ingredient_id
@@ -18,5 +11,4 @@ def units_by_ingredient(request, ingredient_id=None):
     unit_choices = get_unit_choices(supporting_measures=[ingredient.measure_obj])
     return JsonResponse({
         "unit_choices": list(unit_choices),
-        "i": ingredient.id,
     })
