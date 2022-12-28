23922c1771d8c4cd30d74ebb3439bc8e264fed2b
Anuj Agarwal <anuj.ism@gmail.com>
2022-12-28 12:35:39 +0530

    Update factories.py

--- core/factories.py
+++ core/factories.py
@@ -5,6 +5,7 @@ from core.models import Ingredient
 
 class IngredientFactory(factory.django.DjangoModelFactory):
     name = factory.Sequence(lambda n: f"Ingredient {n}")
+    some_other_field = 10
     article_number = factory.Sequence(lambda n: f"Ingredient Number {n}")
 
     class Meta:
