feaa1753eb9f689d730d50f0f2529326ccfe48ec
Anuj Agarwal <anuj.ism@gmail.com>
2023-03-30 13:27:16 +0530

    Update new_factories.py

--- core/new_factories.py
+++ core/new_factories.py
@@ -4,6 +4,7 @@ from core.models import Ingredient
 
 
 class IngredientFactory(factory.django.DjangoModelFactory):
+    some_change_added = factory.Sequence(lambda n: f"Some Change {n}")
     name = factory.Sequence(lambda n: f"Ingredient {n}")
     article_number = factory.Sequence(lambda n: f"Ingredient Number {n}")
 
